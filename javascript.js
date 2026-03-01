5a760f6541198d74763fbe454b2cae0cec1d3be9
WolftheE <145545801+WolftheE@users.noreply.github.com>
2026-03-01 23:11:19 +1100

    Recoded the website from scrach
    Added cool background
    Changed overall layout
    Added dialogs
    Added stuff and yeah

---
+++ javascript.js
@@ -0,0 +1,13 @@
+function showmodal(newText) {
+    document.getElementById('modaltext').innerText = newText;
+    document.getElementById('modal').showModal();
+    document.getElementById('modal').classList.add('animated')
+}
+
+function closemodal() {
+  document.getElementById('modal').close()
+  document.getElementById('modal').classList.remove('animated')
+}
+
+
+
