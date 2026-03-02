8b4cfd473e51b063e34ec7de1ee346afae9c1339
WolftheE <145545801+WolftheE@users.noreply.github.com>
2026-03-02 14:04:44 +1100

    added easter egg like on the old website

--- javascript.js
+++ javascript.js
@@ -8,3 +8,10 @@ function closemodal() {
   document.getElementById('modal').close()
   document.getElementById('modal').classList.remove('animated')
 }
+
+function bug() {
+  document.getElementById('modal').showModal();
+  document.getElementById('modaltext').innerText = 'Why did you click on me? Well too late to change now, I WILL infect everything!';
+  document.getElementById('blob').classList.add('blob_bug')
+  document.getElementById('pfp').src = "images/null.png";
+}
