437045c9e61afe9555627f5d0b6a705e01282720
WolftheE <145545801+WolftheE@users.noreply.github.com>
2026-03-22 21:53:14 +1100

    Adds my minecraft server website lol

---
+++ kermitkrack/morejavascript.js
@@ -0,0 +1,6 @@
+// why do i need javascript to do everything?
+// who ever created javascript please, don't ever create anything again!
+
+function changedocument(newfile) {
+    document.getElementById('docuement-viewer').src = newfile;
+}
