000b5ad844f7eb7d514a83148a447b311d7e7e95
WolftheE <145545801+WolftheE@users.noreply.github.com>
2026-04-05 11:36:34 +1000

    Remove the KK wiki as it will be hosted by someone else

--- kermitkrack/morejavascript.js
+++
@@ -1,6 +0,0 @@
-// why do i need javascript to do everything?
-// who ever created javascript please, don't ever create anything again!
-
-function changedocument(newfile) {
-    document.getElementById('docuement-viewer').src = newfile;
-}
